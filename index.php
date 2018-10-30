b1460b540104063083954879dbe0fcb9f124b422
Eliot Cholerton-Hill <eliot.cholerton-hill@corp.20i.com>
2018-10-30 11:37:48 +0000

    Update

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <body>
 <h1>PHP Page</h1>
 
+
+
 <?php
-echo "Hello World!";
+$my_variable = "Hello world";
+echo "I want to say $my_variable<br>";
+echo "And also " . $my_variable . " again.";
 ?>
 
 </body>
